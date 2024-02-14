d1a050a0a13c4e943e9a47b56f52428ac9136de2
Ernests <austrins.ernests@gmail.com>
2024-02-14 07:49:45 +0000

    created new package excercise

---
+++ src/main/java/rvt/excercises/Person.java
@@ -0,0 +1,35 @@
+package rvt.excercises;
+
+public class Person {
+    //username
+    public String name;
+    //adress
+    public String address;
+
+    public Person (String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String setName(String name){
+        return this.name = name;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public String setAddress(String address){
+        return this.address = address;
+    }
+
+    public String toString(){
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                "}";
+    }
+
+    
+}
---
+++ src/main/java/rvt/excercises/Student.java
@@ -0,0 +1,14 @@
+package rvt.excercises;
+
+public class Student extends Person{
+    public String credits;
+
+    public Student(String name, String address) {
+        super(name, address);
+        this.credits = "0";
+
+        
+    }
+    
+
+}
